6f99ad72daadb6e67509759853e00e03903f582c
tmasters <tmasters@thoughtworks.com>
2019-06-01 20:48:42 +0100

    add setup.py and try to make into a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geexarray',
+      version='0.1',
+      description='Convert earth engine collection objects to xarrays',
+      url='https://github.com/tam-borine/GEExarray',
+      author='Tam Borine and Tommy Lees',
+      author_email='',
+      license='MIT',
+      packages=['geexarray'],
+      zip_safe=False)
