42bcf6d3dc9d316ad3a1f41dce0db192e34fd1cd
tommylees112 <thomas.lees112@gmail.com>
2019-06-01 16:01:41 +0100

    initial tests

---
+++ tests/test1.py
@@ -0,0 +1,23 @@
+import ee
+
+ee.Initialize()
+
+#
+ETHIOPIA_POLYGON = [[33.226164129944436, 3.1516405655312387],
+                    [49.222257879944436, 3.0638792066536986],
+                    [49.178312567444436, 15.527940133202428],
+                    [31.556242254944436, 14.849380988829536],
+                    [33.226164129944436, 3.1516405655312387]]
+
+ethiopia_polygon = ee.Geometry.Polygon(ETHIOPIA_POLYGON)
+
+# s1data = ee.ImageCollection('COPERNICUS/S1_GRD')
+
+modisdata = (
+    ee.ImageCollection('MODIS/006/MOD09A1')
+    .filterDate('2002-12-31','2016-8-4')
+)
+
+#
+def _(ImageCollection: str, year: List[int]):
+    return
---
+++ tests/tfrecord_to_xarray.py
@@ -0,0 +1,24 @@
+import xarray as xr
+import tensorflow as tf
+from pathlib import Path
+
+def get_values(file):
+  data = []
+  for example in tf.python_io.tf_record_iterator(file, options=options):
+    result = tf.train.Example.FromString(example)
+    is_flooded_example = get_value_from_Example(result, 'constant') == 1.0
+    data.append([get_value_from_Example(result, f) for f in features])
+  return data
+
+file = '/Users/tommylees/Downloads/EMSR122_01STRYMONAS_01DELINEATION_MONIT03_v1_75000_area_of_interest.tfrecord'
+options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
+
+
+Path(file).exists()
+
+for example in tf.python_io.tf_record_iterator(file):
+    result = tf.train.Example.FromString(example)
+    print(result)
+
+for example in tf.python_io.tf_record_iterator(file):
+    result = tf.train.Example.FromString(example)
