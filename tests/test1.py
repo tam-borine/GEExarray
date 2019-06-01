c0c5ec4891c10f2694c7f49cbbac8bcc20f65a6d
tmasters <tmasters@thoughtworks.com>
2019-06-01 18:36:00 +0100

    add export to tfrecord functions

--- tests/test1.py
+++ tests/test1.py
@@ -1,23 +1,23 @@
 import ee
+import export_tfrecord_to_GCS as GEExarray
 
 ee.Initialize()
 
-
-ETHIOPIA_POLYGON = [[33.226164129944436, 3.1516405655312387],
-                    [49.222257879944436, 3.0638792066536986],
-                    [49.178312567444436, 15.527940133202428],
-                    [31.556242254944436, 14.849380988829536],
-                    [33.226164129944436, 3.1516405655312387]]
+ETHIOPIA_POLYGON = [[37.93497660463652,8.022690923975954],
+                    [39.27530863588652,7.979173384831735],
+                    [39.53898051088652,8.261953466694475],
+                    [37.97892191713652,8.39239945580339],
+                    [37.93497660463652,8.022690923975954]]
 
 ethiopia_polygon = ee.Geometry.Polygon(ETHIOPIA_POLYGON)
-
-# s1data = ee.ImageCollection('COPERNICUS/S1_GRD')
 
 modisdata = (
     ee.ImageCollection('MODIS/006/MOD09A1')
     .filterDate('2002-12-31','2016-8-4')
     .clip()
 )
+
+GEExarray.export_to_tfrecord(modisdata, ethiopia_polygon)
 
 
 
