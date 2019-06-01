e24df16d1ffa99ede1db1c112bb586c2feabfddc
tommylees112 <thomas.lees112@gmail.com>
2019-06-01 16:34:35 +0100

    update tests to include example dataset

--- tests/test1.py
+++ tests/test1.py
@@ -18,4 +18,20 @@ modisdata = (
     .filterDate('2002-12-31','2016-8-4')
 )
 
-#
+# to produce this as an example!
+time = pd.date_range('2002-12-31','2016-8-4',freq='8D')
+lon = np.linspace(31.556242254944436, 49.178312567444436, 100)
+lat = np.linspace(3.1516405655312387, 15.527940133202428, 100)
+band1 = np.random.randint(0, 10, size=(len(time), len(lat), len(lon)))
+band2 = np.random.randint(0, 10, size=(len(time), len(lat), len(lon)))
+band3 = np.random.randint(0, 10, size=(len(time), len(lat), len(lon)))
+
+eg_ds = xr.Dataset(
+    {'band1': (['time', 'lon', 'lat'], band1),
+     'band2': (['time', 'lon', 'lat'], band2),
+     'band3': (['time', 'lon', 'lat'], band3)},
+    coords={
+        'time': time,
+        'lon': lon,
+        'lat': lat,}
+)
