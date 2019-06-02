88f1173c7812da1235816853aca368aba0ba0915
tmasters <tmasters@thoughtworks.com>
2019-06-02 06:23:21 +0100

    change region from ethiopia to utah in landsat example

--- landsat_example.py
+++ landsat_example.py
@@ -1,22 +1,22 @@
 import ee
-from geexarray.lib.export_tfrecord_to_GCS import export_to_tfrecord
+from geexarray.api import GEEXarray
 
 ee.Initialize()
+gx = GEEXarray()
 
-ETHIOPIA_POLYGON = [[37.93497660463652,8.022690923975954],
-                   [39.27530863588652,7.979173384831735],
-                   [39.53898051088652,8.261953466694475],
-                   [37.97892191713652,8.39239945580339],
-                   [37.93497660463652,8.022690923975954]]
+UTAH_POLYGON = [
+    [-114.53593749999999,39.21923019881908],
+    [-109.61406249999999,34.29325621656583],
+    [-109.70195312499999,40.634547146777386],
+     [-114.53593749999999,39.21923019881908]
+    ]
 
-ethiopia_polygon = ee.Geometry.Polygon(ETHIOPIA_POLYGON)
+bounds = ee.Geometry.Polygon(UTAH_POLYGON)
 
-landsat_collection = ee.ImageCollection('LANDSAT/LE07/C01/T1').filterDate('2000-06-01', '2000-06-11').filterBounds(ethiopia_polygon)
-
+landsat_collection = ee.ImageCollection('LANDSAT/LE07/C01/T1').filterDate('2000-06-01', '2000-06-11')
+modisdata = ee.ImageCollection('MODIS/006/MOD09A1').filterDate('2002-12-31','2016-8-4')
 # a user should just be able to say to_xarray(collection)
 
-geexarray.to_xarray()
-
-export_to_tfrecord(collection_to_export, ethiopia_polygon)
+gx.to_xarray(modisdata, bounds)
 
 # once it has exported we want to pull convert
