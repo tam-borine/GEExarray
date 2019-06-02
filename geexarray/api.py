8dc4e10b8beb7bc34a9e6bbe59d2fa53e2fb5366
tmasters <tmasters@thoughtworks.com>
2019-06-02 05:24:27 +0100

    make main api class

--- geexarray/api.py
+++ geexarray/api.py
@@ -1,7 +1,29 @@
-from geexarray.lib.export_tfrecord_to_GCS import export_to_tfrecord
-from geexarray.lib.tfrecord_to_xarray import 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from google.cloud import storage
 
-def to_xarray(collection,bounds):
-    export_to_tfrecord(collection,bounds)
-    #when it is done.
-    
+from geexarray.lib.export_tfrecord_to_GCS import export_to_tfrecord
+
+# from geexarray.lib.tfrecord_to_xarray import
+
+class GEEXarray:
+
+    def __init__(self, bucket_name="geexarray", gcloud_creds="/Disso-e55e8b924bcf.json"):
+        """ Create a new converter """
+        self.bucket = bucket_name
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getcwd()+gcloud_creds
+
+    def __check_upload(self,name):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.bucket)
+        stats = storage.Blob(bucket=bucket, name=name).exists(storage_client)
+        return stats
+
+    def to_xarray(self, collection, bounds):
+        file_name_prefix = export_to_tfrecord(collection,bounds, self.bucket)
+        name = file_name_prefix+"mixer.json"
+        # check export is done then call tfrecord to xarray module.
+        stats = self.__check_upload(name)
+        print(stats, "stats! ")
+
+        
